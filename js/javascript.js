9b4a16b92973563a6e9da56e1a990e51dcf1f938
IngeDW <58260360+IngeDW@users.noreply.github.com>
2020-01-18 15:22:55 +0100

    Add files via upload

--- js/javascript.js
+++ js/javascript.js
@@ -4,19 +4,61 @@
 
 /*eslint 'no-console': 0*/
 
-
 document.documentElement.classList.remove("no-js");
 document.documentElement.classList.add("js");
 
 
 
-var hart = document.querySelector(".leeg-hartje");
+/*hartjes liken*/
+
+var hartjes = document.querySelectorAll('.leeg-hartje');
+console.log(hartjes);
+
+hartjes.forEach(function(hart) {
+    hart.addEventListener('click', function() {
+        hart.src = "images/hart-vol.svg";
+    })
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/*
+
+        hart.classlist.add('fill');
+
+
 
 function changeHart() {
-    hart.src = "../images/hart-vol.svg";
+    hart.src = "images/hart-vol.svg";
     console.log("er is geklikt");
 }
 
-hart.addEventListener('click', changeHart);
+hart[0].addEventListener('click', changeHart);
 
 
+
+for (var i = 0; i < hart.length; i++) {
+    hart.src = "images/hart-vol.svg";
+    console.log("er is geklikt");
+}
+
+var inputVakantieFilter = document.getElementById("value").value; 
+var tussenkop = document.getElementsByTagName("h4");
+
+function updateTussenkop() {
+tussenkop.write(inputVakantieFilter);
+}
+
+
+*/
